7bd7fc53595d55b0c2191ffc9a39375be5930cc5
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-14 16:58:06 +0300

    change Design, make relatives

--- CatchTheKenny/ViewController.swift
+++ CatchTheKenny/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var kennyImage: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var containView: UIView!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
