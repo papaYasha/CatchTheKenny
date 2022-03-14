1bd296b055b748ee881848cdbf7367d11bdecacb
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-14 18:51:37 +0300

    Add ImageView programatically

--- CatchTheKenny/ViewController.swift
+++ CatchTheKenny/ViewController.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var kennyImage: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var containView: UIView!
     @IBOutlet weak var highScoreLabel: UILabel!
     
+    var kennyImage = UIImageView()
     var timeCounter = 30
     var score = 0
     var highScore = 0
     var timer = Timer()
+    let recognizer = UITapGestureRecognizer(target: self, action: #selector(increaseScore))
 
     
     override func viewDidLoad() {
@@ -36,11 +37,27 @@ class ViewController: UIViewController {
     }
     
     func configKennyImage() {
+        kennyImage.image = UIImage(named: "kenny")
+        kennyImage.frame.size = CGSize(width: 70, height: 100)
+        kennyImage.frame = CGRect(
+            x: containView.center.x - 35,
+            y: containView.center.y - 50,
+            width: kennyImage.frame.width,
+            height: kennyImage.frame.height)
+        containView.addSubview(kennyImage)
         kennyImage.isUserInteractionEnabled = true
+        kennyImage.addGestureRecognizer(recognizer)
     }
     
     func animateImage() {
-        
+//        if kennyImage.center.x >= containView.frame.minX + 50 && kennyImage.center.x <= containView.frame.maxX - 50 && kennyImage.center.y >= containView.frame.minY + 50 && kennyImage.center.y <= containView.frame.maxY - 50 {
+        UIView.animate(withDuration: 0.1) {
+            self.kennyImage.frame = CGRect(
+                x: .random(in: self.containView.frame.minX + 20...self.containView.frame.maxX - 100),
+                y: .random(in: self.containView.frame.minY + 50...self.containView.frame.maxY - 100),
+                width: self.kennyImage.frame.width,
+                height: self.kennyImage.frame.height)
+        }
     }
     
     func showAlert() {
@@ -53,6 +70,7 @@ class ViewController: UIViewController {
     @IBAction func imagePressed(_ sender: UITapGestureRecognizer) {
         score += 1
         scoreLabel.text = "Score: \(score)"
+        animateImage()
     }
     
     @objc func countDown() {
@@ -64,6 +82,11 @@ class ViewController: UIViewController {
             score = 0
             scoreLabel.text = "Score: \(score)"
         }
+    }
+    
+    @objc func increaseScore() {
+        score += 1
+        scoreLabel.text = "Score: \(score)"
     }
 }
 
