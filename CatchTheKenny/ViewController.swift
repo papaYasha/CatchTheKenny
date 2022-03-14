db9953c920da1910dba73c919aa7e5cb240bbe07
papaYasha <90447243+papaYasha@users.noreply.github.com>
2022-03-14 16:39:47 +0300

    Init project, create design

---
+++ CatchTheKenny/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CatchTheKenny
+//
+//  Created by Macbook on 14.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
